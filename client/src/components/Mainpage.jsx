e5f50cb9130289741565e46194bc9ebca8e92bf8
Adeyemi <adeyemiibrahim990@gmail.com>
2025-04-06 15:48:27 -0700

    Adding the percentage

--- client/src/components/Mainpage.jsx
+++ client/src/components/Mainpage.jsx
@@ -1105,7 +1105,7 @@ const Mainpage = () => {
                         <div>Profit:</div>
                       </div>
                       <div className= "text-right text-[30px] sm:text-[39px] text-[#2db9ff] font-bold leading-normal ">
-                        51.00%
+                        51.00%%%
                       </div>
                     </div>
                   </div>
